16eeb919d470ecb2d8c006df852d129a3daee2bb
attackoncs <2710651793@qq.com>
2022-07-18 19:10:05 +0800

    generate proto file's server code

---
+++ cmd/publish/handler.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"MyDouyin/kitex_gen/publish"
+	"context"
+)
+
+// PublishSrvImpl implements the last service interface defined in the IDL.
+type PublishSrvImpl struct{}
+
+// PublishAction implements the PublishSrvImpl interface.
+func (s *PublishSrvImpl) PublishAction(ctx context.Context, req *publish.DouyinPublishActionRequest) (resp *publish.DouyinPublishActionResponse, err error) {
+	// TODO: Your code here...
+	return
+}
+
+// PublishList implements the PublishSrvImpl interface.
+func (s *PublishSrvImpl) PublishList(ctx context.Context, req *publish.DouyinPublishListRequest) (resp *publish.DouyinPublishListResponse, err error) {
+	// TODO: Your code here...
+	return
+}
