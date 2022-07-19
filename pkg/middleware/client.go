352ae603e2aa6bfc3a45f1c1a928746dd74d44d8
attackoncs <2710651793@qq.com>
2022-07-19 17:06:35 +0800

    continue complete pkg:jwt、middleware、ttviper

---
+++ pkg/middleware/client.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"context"
+	"github.com/cloudwego/kitex/pkg/endpoint"
+	"github.com/cloudwego/kitex/pkg/klog"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+)
+
+var _ endpoint.Middleware = ClientMiddleware
+
+func ClientMiddleware(next endpoint.Endpoint) endpoint.Endpoint {
+	return func(ctx context.Context, req, resp interface{}) (err error) {
+		ri := rpcinfo.GetRPCInfo(ctx)
+		klog.Infof("server address: %v, rpc timeout: %v, readwrite timeout: %v", ri.To().Address(), ri.Config().RPCTimeout(), ri.Config().ConnectTimeout())
+		if err = next(ctx, req, resp); err != nil {
+			return err
+		}
+		return nil
+	}
+}
