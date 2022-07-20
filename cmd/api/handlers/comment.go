33f8fcbfeb016c4f778d0d2d9d7f5c5bffa43161
attackoncs <2710651793@qq.com>
2022-07-20 21:17:34 +0800

    update api handlers

---
+++ cmd/api/handlers/comment.go
@@ -0,0 +1,92 @@
+package handlers
+
+import (
+	"context"
+	"strconv"
+
+	"MyDouyin/pkg/errno"
+
+	"MyDouyin/dal/pack"
+	"MyDouyin/kitex_gen/comment"
+
+	"MyDouyin/cmd/api/rpc"
+
+	"github.com/gin-gonic/gin"
+)
+
+// 传递 评论操作 的上下文至 Comment 服务的 RPC 客户端, 并获取相应的响应.
+func CommentAction(c *gin.Context) {
+	var paramVar CommentActionParam
+	token := c.Query("token")
+	video_id := c.Query("video_id")
+	action_type := c.Query("action_type")
+
+	vid, err := strconv.Atoi(video_id)
+	if err != nil {
+		SendResponse(c, pack.BuildCommentActionResp(errno.ErrBind))
+		return
+	}
+	act, err := strconv.Atoi(action_type)
+	if err != nil {
+		SendResponse(c, pack.BuildCommentActionResp(errno.ErrBind))
+		return
+	}
+
+	paramVar.Token = token
+	paramVar.VideoId = int64(vid)
+	paramVar.ActionType = int32(act)
+
+	rpcReq := comment.DouyinCommentActionRequest{
+		VideoId:    paramVar.VideoId,
+		Token:      paramVar.Token,
+		ActionType: paramVar.ActionType,
+	}
+
+	if act == 1 {
+		comment_text := c.Query("comment_text")
+		rpcReq.CommentText = &comment_text
+	} else {
+		comment_id := c.Query("comment_id")
+		cid, err := strconv.Atoi(comment_id)
+		if err != nil {
+			SendResponse(c, pack.BuildCommentActionResp(errno.ErrBind))
+			return
+		}
+		cid64 := int64(cid)
+		rpcReq.CommentId = &cid64
+	}
+
+	resp, err := rpc.CommentAction(context.Background(), &rpcReq)
+	if err != nil {
+		SendResponse(c, pack.BuildCommentActionResp(errno.ConvertErr(err)))
+		return
+	}
+	SendResponse(c, resp)
+}
+
+// 传递 获取评论列表操作 的上下文至 Comment 服务的 RPC 客户端, 并获取相应的响应.
+func CommentList(c *gin.Context) {
+	var paramVar CommentListParam
+	videoid, err := strconv.Atoi(c.Query("video_id"))
+	if err != nil {
+		SendResponse(c, pack.BuildCommentListResp(errno.ErrBind))
+		return
+	}
+	paramVar.VideoId = int64(videoid)
+	paramVar.Token = c.Query("token")
+
+	if len(paramVar.Token) == 0 || paramVar.VideoId < 0 {
+		SendResponse(c, pack.BuildCommentListResp(errno.ErrBind))
+		return
+	}
+
+	resp, err := rpc.CommentList(context.Background(), &comment.DouyinCommentListRequest{
+		VideoId: paramVar.VideoId,
+		Token:   paramVar.Token,
+	})
+	if err != nil {
+		SendResponse(c, pack.BuildCommentListResp(errno.ConvertErr(err)))
+		return
+	}
+	SendResponse(c, resp)
+}
